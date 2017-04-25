d05a9fbe07a604f4505cb96ce73c5b69f8391d76
Adrien Grand <jpountz@gmail.com>
2017-04-25 19:20:32 +0200

    Add `terms` queries to the `geonames` track. (#20)
    This adds `terms` queries with 30k terms used in various configurations to the
    `geonames` dataset so that we are more likely to know about regressions in the
    future.

---
+++ geonames/track.py
@@ -0,0 +1,107 @@
+import random
+import os
+
+class QueryParamSource:
+
+    def __init__(self, indices, params):
+        self._indices = indices
+        self._params = params
+        cwd = os.path.dirname(__file__)
+        # The terms.txt file has been generated with:
+        # sed -n '13~250p' [path_to_rally_data]/geonames/documents.json | shuf | sed -e "s/.*name\": \"//;s/\",.*$//" > terms.txt
+        with open(os.path.join(cwd, "terms.txt"), "r") as ins:
+            self.terms = [line.strip() for line in ins.readlines()]
+
+    def partition(self, partition_index, total_partitions):
+        return self
+
+    def size(self):
+        return 1
+
+class PureTermsQueryParamSource(QueryParamSource):
+
+    def params(self):
+        query_terms = list(self.terms) # copy
+        query_terms.append(str(random.randint(1,100))) #avoid caching
+        result = {
+            "body": {
+                "query": {
+                    "terms": {
+                        "name.raw": query_terms
+                    }
+                }
+            },
+            "index": None,
+            "type": None,
+            "use_request_cache": self._params.get("use_request_cache", False)
+        }
+        return result
+
+class FilteredTermsQueryParamSource(QueryParamSource):
+
+    def params(self):
+        query_terms = list(self.terms) # copy
+        query_terms.append(str(random.randint(1,1000))) # avoid caching
+        result = {
+            "body": {
+                "query": {
+                    "bool": {
+                      "must": [
+                        {
+                          "match": {
+                            "feature_class.raw": "T"
+                          }
+                        }
+                      ],
+                      "filter": [
+                        {
+                          "terms": {
+                            "name.raw": query_terms
+                          }
+                        }
+                      ]
+                    }
+                }
+            },
+            "index": None,
+            "type": None,
+            "use_request_cache": self._params.get("use_request_cache", False)
+        }
+        return result
+
+class ProhibitedTermsQueryParamSource(QueryParamSource):
+
+    def params(self):
+        query_terms = list(self.terms) # copy
+        query_terms.append(str(random.randint(1,1000))) # avoid caching
+        result = {
+            "body": {
+                "query": {
+                    "bool": { 
+                      "must": [
+                        {
+                          "match": {
+                            "feature_class.raw": "A"
+                          }
+                        }
+                      ],
+                      "must_not": [
+                        {
+                          "terms": {
+                            "name.raw": query_terms
+                          }
+                        }
+                      ]
+                    }
+                }
+            },
+            "index": None,
+            "type": None,
+            "use_request_cache": self._params.get("use_request_cache", False)
+        }
+        return result
+
+def register(registry):
+    registry.register_param_source("pure-terms-query-source", PureTermsQueryParamSource)
+    registry.register_param_source("filtered-terms-query-source", FilteredTermsQueryParamSource)
+    registry.register_param_source("prohibited-terms-query-source", ProhibitedTermsQueryParamSource)
