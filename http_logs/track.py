0233ab15ff063b61fe76140cdbbd8704891ad1a5
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-02-21 11:01:13 +0100

    Use async runners if needed (#97)
    Due to elastic/rally#852 we will implement a compatibility layer in the
    current load generator that will also use the asyncio API and thus
    requires custom runners to be registered differently (by specifying
    `async_runner=True`). Rally's runner registry will also expose a new
    attribute `async_runner` that is set to `True` if Rally requires runners
    to be registered as described above.
    
    With this commit we introduce a (temporary) compatibility layer for all
    custom runners that allows older Rally versions to work with the classic
    runners and newer Rally versions with the async runners.
    
    Relates elastic/rally#852
    Relates elastic/rally#916

--- http_logs/track.py
+++ http_logs/track.py
@@ -1,6 +1,16 @@
 def reindex(es, params):
-  result = es.reindex(body=params.get("body"), request_timeout=params.get("request_timeout"))
-  return result["total"], "docs"
+    result = es.reindex(body=params.get("body"), request_timeout=params.get("request_timeout"))
+    return result["total"], "docs"
+
+
+async def reindex_async(es, params):
+    result = await es.reindex(body=params.get("body"), request_timeout=params.get("request_timeout"))
+    return result["total"], "docs"
+
 
 def register(registry):
-  registry.register_runner("reindex", reindex)
+    async_runner = registry.meta_data.get("async_runner", False)
+    if async_runner:
+        registry.register_runner("reindex", reindex_async, async_runner=True)
+    else:
+        registry.register_runner("reindex", reindex)
