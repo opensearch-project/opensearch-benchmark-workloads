44cfc28c02089976ad6cdac9c046830adf58611d
Marios Trivyzas <matriv@gmail.com>
2020-11-12 16:17:15 +0100

    Add EQL track (#140)
    Add a new track to benchmark EQL performance.
    
    Because of some sporadic errors with downloading the data the bucket url is
    used is not the standard one, issue tracked in https://github.com/elastic/infra/issues/24746

---
+++ eql/track.py
@@ -0,0 +1,12 @@
+import logging
+
+async def eql(es, params):
+    logger = logging.getLogger(__name__)
+    logger.info("Provided params [%s].", params)
+    await es.eql.search(
+            params.get("index"),
+            body=params.get("body"),
+        )
+
+def register(registry):
+    registry.register_runner("eql", eql, async_runner=True)
