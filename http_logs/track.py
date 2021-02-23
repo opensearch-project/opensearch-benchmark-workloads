3b78e941fb9fdbb58b99f2257dfb7efbd1c4b6ac
Nik Everett <nik9000@gmail.com>
2021-02-23 13:46:23 -0500

    Add grok and dissect to runtime fields in http_logs (#156)
    This adds performance tests for runtime fields implemented with grok
    and dissect to the ones that we already have in the http_logs track.
    You control this with the new runtime_fields track parameter. You can
    set it to grok, dissect, or index-of to get runtime fields powered
    with those functions.

--- http_logs/track.py
+++ http_logs/track.py
@@ -1,3 +1,9 @@
+from copy import copy
+import re
+
+from esrally import exceptions
+from esrally.track import loader
+
 def reindex(es, params):
     result = es.reindex(body=params.get("body"), request_timeout=params.get("request_timeout"))
     return result["total"], "docs"
@@ -7,6 +13,31 @@ async def reindex_async(es, params):
     result = await es.reindex(body=params.get("body"), request_timeout=params.get("request_timeout"))
     return result["total"], "docs"
 
+class RuntimeFieldResolver(loader.TrackProcessor):
+    PATTERN = re.compile('.+-from-(.+)-using-(.+)')
+
+    def on_after_load_track(self, t):
+        for challenge in t.challenges:
+            for task in challenge.schedule:
+                m = self.PATTERN.match(task.name)
+                if m is not None:
+                    task.operation = copy(task.operation)
+                    task.operation.params = self._replace_field(f"{m[2]}.from_{m[1]}.", task.operation.params)
+
+    def on_prepare_track(self, track, data_root_dir):
+        return True
+
+    def _replace_field(self, field, t):
+        if t == 'path' or t == 'status':
+            return field + t
+        if isinstance(t, list):
+            return [self._replace_field(field, v) for v in t]
+        if isinstance(t, dict):
+            return {
+                self._replace_field(field, k): self._replace_field(field, v)
+                for k, v in t.items()
+            }
+        return t
 
 def register(registry):
     async_runner = registry.meta_data.get("async_runner", False)
@@ -14,3 +45,4 @@ def register(registry):
         registry.register_runner("reindex", reindex_async, async_runner=True)
     else:
         registry.register_runner("reindex", reindex)
+    registry.register_track_processor(RuntimeFieldResolver())
