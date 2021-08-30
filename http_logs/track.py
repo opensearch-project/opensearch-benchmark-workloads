8739314e4c38d937d30df3775d74878515dd778f
Ian Hoang <hoangia@amazon.com>
2021-08-30 16:36:08 -0500

    Updated challenge to test_procedures
    Signed-off-by: Ian Hoang <hoangia@amazon.com>

--- http_logs/track.py
+++ http_logs/track.py
@@ -19,8 +19,8 @@ class RuntimeFieldResolver(loader.TrackProcessor):
     PATTERN = re.compile('.+-from-(.+)-using-(.+)')
 
     def on_after_load_track(self, t):
-        for challenge in t.challenges:
-            for task in challenge.schedule:
+        for test_procedure in t.test_procedures:
+            for task in test_procedure.schedule:
                 m = self.PATTERN.match(task.name)
                 if m is not None:
                     source = m[1]
