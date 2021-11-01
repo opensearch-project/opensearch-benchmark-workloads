3f39bafd057f0059d4213ab14b311fa303a40375
Travis Benedict <benedtra@amazon.com>
2021-11-01 09:59:02 -0500

    Update pmc import to use osbenchmark (#15)
    Signed-off-by: Travis Benedict <benedtra@amazon.com>

--- pmc/workload.py
+++ pmc/workload.py
@@ -5,6 +5,6 @@ def put_settings(es, params):
 def register(registry):
     # register a fallback for older Benchmark versions
     try:
-        from esrally.worker_coordinator.runner import PutSettings
+        from osbenchmark.worker_coordinator.runner import PutSettings
     except ImportError:
         registry.register_runner("put-settings", put_settings)
