037d85bf3b4e2fefa51d6994e478481df0b32802
Ian Hoang <hoangia@amazon.com>
2022-02-04 16:39:05 -0600

    Http Logs Fix for OSB Issue (#106) https://github.com/opensearch-project/opensearch-benchmark/issues/106
    Signed-off-by: Ian Hoang <hoangia@amazon.com>

--- http_logs/workload.py
+++ http_logs/workload.py
@@ -1,10 +1,3 @@
-from copy import copy
-import re
-
-from osbenchmark import exceptions
-from osbenchmark.workload import loader
-
-
 def reindex(es, params):
     result = es.reindex(body=params.get("body"), request_timeout=params.get("request_timeout"))
     return result["total"], "docs"
@@ -15,52 +8,9 @@ async def reindex_async(es, params):
     return result["total"], "docs"
 
 
-class RuntimeFieldResolver(loader.WorkloadProcessor):
-    PATTERN = re.compile('.+-from-(.+)-using-(.+)')
-
-    def on_after_load_workload(self, t):
-        for test_procedure in t.test_procedures:
-            for task in test_procedure.schedule:
-                m = self.PATTERN.match(task.name)
-                if m is not None:
-                    source = m[1]
-                    impl = m[2].replace('-', '_')
-                    task.operation = copy(task.operation)
-                    task.operation.params = self._replace_field(f"{impl}.from_{source}.", task.operation.params)
-
-    def on_prepare_workload(self, workload, data_root_dir):
-        class EmptyTrueList(list):
-            def __bool__(self):
-                return True
-
-            def __eq__(self, other):
-                if isinstance(other, bool):
-                    return True
-
-        return EmptyTrueList()
-
-    def _replace_field(self, field, t):
-        if t == 'path' or t == 'status':
-            return field + t
-        if isinstance(t, list):
-            return [self._replace_field(field, v) for v in t]
-        if isinstance(t, dict):
-            return {
-                self._replace_field(field, k): self._replace_field(field, v)
-                for k, v in t.items()
-            }
-        return t
-
-
 def register(registry):
     async_runner = registry.meta_data.get("async_runner", False)
     if async_runner:
         registry.register_runner("reindex", reindex_async, async_runner=True)
     else:
         registry.register_runner("reindex", reindex)
-    registry.register_workload_processor(RuntimeFieldResolver())
-    try:
-        registry.register_workload_processor(loader.DefaultWorkloadPreparator())
-    except TypeError as e:
-        if e == "__init__() missing 1 required positional argument: 'cfg'":
-            pass
